8edc04055ad93659dec2fc0afc283068f0b92c16
l_inzirillo <l_inzirillo@etu-webschoolfactory.fr>
2023-03-13 21:59:11 +0100

    Initiation de l'algo pure JS
    Pour commencer j'ai créer les variables de base du jeux qui comprend les colonnes, les lignes, les éléments vides.
    
    En n'oubliant pas les deux joueurs car ce jeux se joue à deux.
    
    Ensuite il nous faut créer la matrice des grilles vides pour pouvoir placer les jetons ( autre fonction à ajouter )

---
+++ algo.js
@@ -0,0 +1,16 @@
+// Je définit les variables de grille pour le puissance 4
+
+const ROWS = 6;
+const COLS = 7;
+const EMPTY = 0;
+const PLAYER_ONE = 1;
+const PLAYER_TWO = 2;
+
+// Je créer une matrice de grille vide pour pouvoir y placer les jetons
+let grid = [];
+for (let i = 0; i < ROWS; i++) {
+  grid[i] = [];
+  for (let j = 0; j < COLS; j++) {
+    grid[i][j] = EMPTY;
+  }
+}
