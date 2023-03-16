0b25c1adbaef74a4337d1096859e896e5075e877
Ludovic <ludoviclelong10@gmail.com>
2023-03-16 10:13:51 +0100

    Squashed commit of the following:
    commit 92ce6b1695545f6f01bb40ea9cb891b095fc4ff9
    Author: Ludovic <ludoviclelong10@gmail.com>
    Date:   Thu Mar 16 09:17:47 2023 +0100
    
        Add structure and version route, je fais de l'hateoas maintenant
    
    commit 3a8d919620c939c994a7460aad442c5e6f9e06c6
    Author: Ludovic <ludoviclelong10@gmail.com>
    Date:   Wed Mar 15 16:38:27 2023 +0100
    
        Commencement initialisation des routes, étape obliger pour faire hateoas

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
-/**
- * Express App
- */
+/*
 const express = require("express");
 const app = express();
 
 // middleware
 app.use(express.json());
-app.use(("/v1/users", require("./routes/users")));
-app.use(("/v1/game", require("./routes/game")));
+app.use(("/v1/users", require("./routes/V1/users")));
+app.use(("/v1/game", require("./routes/V1/game")));
+*/
 
-module.exports = app;
+export const app = express();
+
+import express from 'express';
+import { createServer } from 'http';
+import { router } from './routes/V1/routes.js';
+
+
+const server = createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    // use router to handle requests
+    router(req, res, () => {});
+
+
+    res.end('Hello World!')
+
+});
+
+server.listen(3001)
+console.log('Server started on port 3001');
+
+
+
+
+
+
