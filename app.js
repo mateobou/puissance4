c53fe60e98291875d1d20e84cdc0f4445f514b3c
Ludovic <ludoviclelong10@gmail.com>
2023-04-23 15:22:02 +0200

    Avance sur le traduction avec le middleware

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 export const app = express();
 import dotenv from 'dotenv';
 import express from 'express';
+/* importer nos middlewares */
 import { getAllRoutes } from './middlewares/hateoas.js';
-import { i18next } from './middlewares/i18n.js';
+import { i18n } from './middlewares/i18n.js';
+
+
+
 
 // initialiser dotenv
 dotenv.config();
 const PORT = process.env.PORT || 3001;
 
 // initialiser i18n
-app.use(i18next);
+app.use(i18n);
 
 
 
-// initialisé link personnalisé middleware
+// initialiser link personnalisé middleware
 app.use(getAllRoutes);
 
 app.get('/', (req, res) => {
     const message = 'Hello World!';
+
+    /* utiliser le middleware traduction key valeur du bon fichier json de langue de la route */ 
+    res.locals.t('Bienvenue');
+
     res.json(message);
 });
 
 app.get('/example', (req, res) => {
     const message = 'Hello World!';
+
+    /* utiliser le middleware traduction key valeur du bon fichier json de langue de la route */ 
+    res.locals.t('Bienvenue');
+
     res.json(message);
 });
 
--- middlewares/i18n.js
+++ middlewares/i18n.js
@@ -1,14 +1,30 @@
 import i18next from "i18next";
+import traductionFr from "../traductions/home/fr.json" assert { type: "json" };
+import traductionEn from "../traductions/home/en.json" assert { type: "json" };
 
 
-
-const languages = ["en", "fr"];
-const directory = `${__dirname}/../locales`;
-const defaultLanguage = "fr";
+i18next.init({
+    lng: "fr",
+    fallbackLng: "fr",
+    debug: true,
+    resources: {
+        fr : {
+            translation:  traductionFr
+        },
+        en: {
+            translation: traductionEn
+        }
+    },
+});
 
 
 export const i18n = (req, res, next) => {
     const locale = req.headers["accept-language"];
     i18next.changeLanguage(locale);
+
+    res.locals.t = i18next.t.bind(i18next);
     next();
 }
+
+
+
