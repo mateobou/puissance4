e1dc9461f17163911d039c15de3eb432fe299aa7
mateodix <mateo@sherpas.com>
2023-05-06 12:07:35 +0200

    Deletion of functions

--- routes/game.js
+++ routes/game.js
@@ -56,36 +56,5 @@ router.post("/Action/:numeroDeColonne", async (req, res) => {// Couleur du joueu
 });
 
 
-router.get("/:id", async (req, res) => {
-  const user = await User.findByPk(req.params.id);
-  if (user) {
-    res.json(user);
-  } else {
-    res.sendStatus(404);
-  }
-});
-
-router.put("/:id", async (req, res) => {
-  const user = await User.findByPk(req.params.id);
-  if (user) {
-    await user.update(req.body);
-    res.json(user);
-  } else {
-    res.sendStatus(404);
-  }
-});
-
-router.delete("/:id", async (req, res) => {
-  const user = await User.findByPk(req.params.id);
-  if (user) {
-    await user.destroy();
-    res.sendStatus(204);
-  } else {
-    res.sendStatus(404);
-  }
-});
-
-
-
 // module.exports = router;
 export default router;
